aa98223284460503de053bf582dc9a9715039ec8
C1neks <baranowskimarcin7@gmail.com>
2022-10-11 14:17:40 +0200

    adding styles

---
+++ src/Weather/Weather.styles.ts
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  background-color: #ec9413;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const TopInfo = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 90%;
+  margin-top: 2rem;
+`;
+
+export const MainInfo = styled.div`
+  width: 90%;
+  display: flex;
+`;
+
+export const WeatherIcon = styled.div`
+  width: 50%;
+`;
+
+export const WeatherInfo = styled.div`
+  width: 50%;
+`;
--- src/Weather/Weather.tsx
+++ src/Weather/Weather.tsx
@@ -2,6 +2,14 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { RiCelsiusLine } from "react-icons/ri";
+import { FiSun } from "react-icons/fi";
+import {
+  MainInfo,
+  TopInfo,
+  WeatherIcon,
+  WeatherInfo,
+  Wrapper,
+} from "./Weather.styles";
 const API_KEY = process.env.REACT_APP_API_KEY;
 
 const Weather = () => {
@@ -35,46 +43,86 @@ const Weather = () => {
   }, []);
 
   return (
-    <>
-      <div>{forecast.resolvedAddress}</div>
-      <div>{today.toDateString()}</div>
-      <div>{forecast.address}</div>
-      {forecast.days.map((x: any, i: number) => (
-        <div key={x.datetimeEpoch}>
-          {i === 0 ? (
-            <div>
-              <h3>Data {x.datetime}</h3>
-              <h4>Actual Temperature {x.temp}</h4>
-              <h4>Max Temperature {x.tempmax}</h4>
-              <h4>Min Temperature {x.tempmin}</h4>
-              <h4>{x.conditions}</h4>
-              <h4>Feels like {x.feelslike}</h4>
-              <h4>{x.icon}</h4>
-              <p>Sunrise: {x.sunrise}</p> <p>Sunset: {x.sunset}</p>{" "}
-              <p>humidity: {x.humidity}</p> <p>{x.windspeed + "km/h"}</p>{" "}
-              <p>Precipitation: {x.precipprob}</p> <p>Pressure: {x.pressure}</p>
-              {x.hours.map((hour: any) => (
-                <div key={hour.datetime}>
-                  <p>
-                    {hour.datetime >= time
-                      ? hour.datetime + " " + hour.temp
-                      : null}
-                  </p>
+    <Wrapper>
+      <TopInfo>
+        <GiHamburgerMenu />
+        <div>{today.toDateString()}</div>
+        <RiCelsiusLine />
+      </TopInfo>
+
+      <MainInfo>
+        <WeatherInfo>
+          {/*<div>{forecast.resolvedAddress}</div>*/}
+          <div>{forecast.address}</div>
+          {forecast.days.map((x: any, i: number) => (
+            <div key={x.datetimeEpoch}>
+              {i === 0 ? (
+                <div>
+                  <h4>Actual Temperature {x.temp}</h4>
+                  <h4>Max Temperature {x.tempmax}</h4>
+                  <h4>Min Temperature {x.tempmin}</h4>
+                  <h4>{x.conditions}</h4>
+                  <h4>Feels like {x.feelslike}</h4>
+                  <h4>{x.icon}</h4>
+                  <p>Sunrise: {x.sunrise}</p> <p>Sunset: {x.sunset}</p>{" "}
+                  <p>humidity: {x.humidity}</p> <p>{x.windspeed + "km/h"}</p>{" "}
+                  <p>Precipitation: {x.precipprob}</p>{" "}
+                  <p>Pressure: {x.pressure}</p>
+                  {x.hours.map((hour: any) => (
+                    <div key={hour.datetime}>
+                      <p>
+                        {hour.datetime >= time
+                          ? hour.datetime + " " + hour.temp
+                          : null}
+                      </p>
+                    </div>
+                  ))}
                 </div>
-              ))}
+              ) : null}
             </div>
-          ) : (
-            <div>
-              <h3>Data {x.datetime}</h3>
-              <h4>{x.icon}</h4>
-              <p>{x.precipprob}</p>
-              <h4>Max Temperature {x.tempmax}</h4>
-              <h4>Min Temperature {x.tempmin}</h4>
-            </div>
-          )}
-        </div>
-      ))}
-    </>
+          ))}
+        </WeatherInfo>
+        <WeatherIcon>
+          <FiSun />
+        </WeatherIcon>
+      </MainInfo>
+
+      {/*{forecast.days.map((x: any, i: number) => (*/}
+      {/*  <div key={x.datetimeEpoch}>*/}
+      {/*    {i === 0 ? (*/}
+      {/*      <div>*/}
+      {/*        <h3>Data {x.datetime}</h3>*/}
+      {/*        <h4>Actual Temperature {x.temp}</h4>*/}
+      {/*        <h4>Max Temperature {x.tempmax}</h4>*/}
+      {/*        <h4>Min Temperature {x.tempmin}</h4>*/}
+      {/*        <h4>{x.conditions}</h4>*/}
+      {/*        <h4>Feels like {x.feelslike}</h4>*/}
+      {/*        <h4>{x.icon}</h4>*/}
+      {/*        <p>Sunrise: {x.sunrise}</p> <p>Sunset: {x.sunset}</p>{" "}*/}
+      {/*        <p>humidity: {x.humidity}</p> <p>{x.windspeed + "km/h"}</p>{" "}*/}
+      {/*        <p>Precipitation: {x.precipprob}</p> <p>Pressure: {x.pressure}</p>*/}
+      {/*        {x.hours.map((hour: any) => (*/}
+      {/*          <div key={hour.datetime}>*/}
+      {/*            <p>*/}
+      {/*              {hour.datetime >= time*/}
+      {/*                ? hour.datetime + " " + hour.temp*/}
+      {/*                : null}*/}
+      {/*            </p>*/}
+      {/*          </div>*/}
+      {/*        ))}*/}
+      {/*      </div>*/}
+      {/*    ) : (*/}
+      {/*      <div>*/}
+      {/*        <h3>Data {x.datetime}</h3>*/}
+      {/*        <h4>{x.icon}</h4>*/}
+      {/*        <p>{x.precipprob}</p>*/}
+      {/*        <h4>Max Temperature {x.tempmax}</h4>*/}
+      {/*        <h4>Min Temperature {x.tempmin}</h4>*/}
+      {/*      </div>*/}
+      {/*    )}*/}
+      {/*  </div>*/}
+      {/*))}*/}
+    </Wrapper>
   );
 };
 
